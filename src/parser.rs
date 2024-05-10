5ef65595f66292434f508fe8306f5f0a2afad207
Lian <liansutil@gmail.com>
2024-05-09 22:47:05 -0300

    write clock in

--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,9 @@
 use itertools::Itertools;
 use regex::Regex;
 use std::fs::{self, File, OpenOptions};
-use std::io::{self, BufRead, BufReader, Lines, Write};
+use std::io::{BufRead, BufReader, Lines, Result as IoResult, Write};
 use std::path::Path;
+use chrono::prelude::*;
 
 // could save text inside the enum maybe
 #[derive(Debug)]
@@ -30,11 +31,11 @@ fn parse_todo(heading: &str) -> Option<Status> {
     None
 }
 
-fn read_lines<P>(filename: P) -> io::Result<Lines<BufReader<File>>>
+fn read_lines<P>(filename: P) -> IoResult<Lines<BufReader<File>>>
 where
     P: AsRef<Path>,
 {
-    let file = File::open(filename)?;
+    let file = File::open(filename).expect("Can't fiind file");
     Ok(BufReader::new(file).lines())
 }
 
@@ -61,6 +62,32 @@ pub fn write_todo_state(todo: &Item) {
     fs::rename("tmp", "notas.org").expect("Error while overwriting");
 }
 
+pub fn write_clock_in(todo: &Item) {
+    let mut f = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("./tmp")
+        .expect("Unable to open file");
+    let dt = Local::now();
+
+    if let Ok(lines) = read_lines("./notas.org") {
+        for (index, line) in lines.enumerate() {
+            if let Ok(line) = line {
+                if (todo.line_number + 1) == index && line.contains(":LOGBOOK:"){
+                    writeln!(&mut f, "{}", line).expect("Unable to write");
+                    writeln!(&mut f, "CLOCK: {}", dt.format("[%Y-%m-%d %a %H:%M]")).expect("Unable to write");
+
+                } else {
+                    writeln!(&mut f, ":LOGBOOK:").expect("Unable to write");
+                    writeln!(&mut f, "CLOCK: {}", dt.format("[%Y-%m-%d %a %H:%M]")).expect("Unable to write");
+                    writeln!(&mut f, ":END:").expect("Unable to write");
+                    writeln!(&mut f, "{}", line).expect("Unable to write");
+                }
+            }
+        }
+    }
+    fs::rename("tmp", "notas.org").expect("Error while overwriting");
+}
 pub fn parse_todos(todos: &mut Vec<Item>) {
     let heading_reg: Regex = Regex::new(r"\*.*").unwrap();
     // TODO pass file as arg and have a default file
